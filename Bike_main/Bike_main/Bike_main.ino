59b14142c344f9636e31c239c20632152a3c487a
raiymbekkabiyev <72629885+raiymbekkabiyev@users.noreply.github.com>
2021-05-29 22:47:00 +0600

    Problem with blinking
    Team, I started programming LED and I am not sure how to make blinking without delay(). Apparently, using delay() would cause problems because the M5Atom board won't be able to perform 2 tasks simultaneously. Could you please look at the code and suggest how we can proceed here?

--- Bike_main/Bike_main/Bike_main.ino
+++ Bike_main/Bike_main/Bike_main.ino
@@ -1,11 +1,34 @@
 #include "M5Atom.h"
+#include <FastLED.h>
+#define BRIGHTNESS 30 //constant value for brightness
+const long interval = 1000; // interval of blinking
 
-void setup() {
-  // put your setup code here, to run once:
-  //Testing GitHub 
+void setup(){
+  M5.begin(true, false, true)
+  FastLED.setBrightness(BRIGHTNESS); setting constant brightness
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint8_t FSM = 0;
 
+void loop(){
+  if(M5.Btn.wasPressed()){
+    
+    switch (FSM){
+      case 0: // LED is black=off 
+        M5.dis.drawpix(0, 0x000000);
+        break; 
+      case 1: // LED is blinking Red 
+        M5.dis.drawpix(0, 0xf00000);
+        break;  
+      
+    }
+
+     FSM++;
+        if (FSM >= 4)
+        {
+            FSM = 0;
+        }
+  }
+  delay(50);
+  M5.update();
 }
