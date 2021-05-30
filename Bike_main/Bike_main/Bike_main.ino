74c9c687a46cd3460b55bf32c18538ac7a50054d
raiymbekkabiyev <72629885+raiymbekkabiyev@users.noreply.github.com>
2021-05-30 16:31:21 +0600

    Added blinking with delay()
    This is main structure of the code. I added blinking using delay(). Next step would be to do it without delay()

--- Bike_main/Bike_main/Bike_main.ino
+++ Bike_main/Bike_main/Bike_main.ino
@@ -1,6 +1,8 @@
 #include "M5Atom.h"
 #include <FastLED.h>
-#define BRIGHTNESS 30 //constant value for brightness
+#define BRIGHTNESS 20 //constant value for brightness
+#define NUM_LEDS 25 
+
 //const long interval = 1000; // interval of blinking
 //const int buttonpin = 39;
 
@@ -8,6 +10,7 @@ float accX = 0, accY = 0, accZ = 0;
 
 void setup() {
   M5.begin(true, false, true);
+  M5.IMU.Init();
   FastLED.setBrightness(BRIGHTNESS); //setting constant brightness
 
   //pinMode(buttonpin, INPUT);
@@ -16,25 +19,32 @@ void setup() {
 uint8_t FSM = 0;
 
 void loop() {
-
+  
+  //M5.IMU.getAccelData(&accX, &accY, &accZ);
   switch (FSM) {
     case 0: // LED is black=off
-      M5.dis.drawpix(0, 0x000000);
+      M5.dis.fillpix(0x000000);
       break;
     case 1: // LED is blinking Red
-      M5.dis.drawpix(0, 0xf00000);
+      M5.dis.fillpix(0x00ff00);
+      delay(10);
+      M5.dis.fillpix(0x000000);
+      delay(50);
       break;
     case 2: //LED is blinkind White 
-      M5.dis.drawpix(0, 0xf00000);
+      M5.dis.fillpix(0xffffff);
+      delay(10);
+      M5.dis.fillpix(0x000000);
+      delay(50);
       break;
-    case 3: //bracking 
-      M5.dis.drawpix(0, 0xf00000);
+    case 3: //bracking REAR RED 
+      M5.dis.fillpix(0x00ff00);
       break;
-    case 4:
-      M5.dis.drawpix(0, 0xf00000);
+    case 4: //bracking FRONT WHITE
+      M5.dis.fillpix(0xffffff);
       break;
 
-      //M5.IMU.getAccelData(&accX, &accY, &accZ);
+      
   }
   if (M5.Btn.wasPressed()) {
     FSM++;
