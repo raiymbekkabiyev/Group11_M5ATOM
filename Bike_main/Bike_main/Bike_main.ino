12d503b49cdbbae2346b6b42a3b84fe3dfa636c4
raiymbekkabiyev <72629885+raiymbekkabiyev@users.noreply.github.com>
2021-05-30 13:44:19 +0600

    Updated main file

--- Bike_main/Bike_main/Bike_main.ino
+++ Bike_main/Bike_main/Bike_main.ino
@@ -1,34 +1,48 @@
 #include "M5Atom.h"
 #include <FastLED.h>
 #define BRIGHTNESS 30 //constant value for brightness
-const long interval = 1000; // interval of blinking
+//const long interval = 1000; // interval of blinking
+//const int buttonpin = 39;
 
-void setup(){
+float accX = 0, accY = 0, accZ = 0;
+
+void setup() {
   M5.begin(true, false, true);
   FastLED.setBrightness(BRIGHTNESS); //setting constant brightness
+
+  //pinMode(buttonpin, INPUT);
 }
 
 uint8_t FSM = 0;
 
-void loop(){
-  if(M5.Btn.wasPressed()){
-    
-    switch (FSM){
-      case 0: // LED is black=off 
-        M5.dis.drawpix(0, 0x000000);
-        break; 
-      case 1: // LED is blinking Red 
-        M5.dis.drawpix(0, 0xf00000);
-        break;  
-      
-    }
+void loop() {
 
-     FSM++;
-        if (FSM >= 4)
-        {
-            FSM = 0;
-        }
+  switch (FSM) {
+    case 0: // LED is black=off
+      M5.dis.drawpix(0, 0x000000);
+      break;
+    case 1: // LED is blinking Red
+      M5.dis.drawpix(0, 0xf00000);
+      break;
+    case 2: //LED is blinkind White 
+      M5.dis.drawpix(0, 0xf00000);
+      break;
+    case 3: //bracking 
+      M5.dis.drawpix(0, 0xf00000);
+      break;
+    case 4:
+      M5.dis.drawpix(0, 0xf00000);
+      break;
+
+      //M5.IMU.getAccelData(&accX, &accY, &accZ);
   }
-  delay(50);
+  if (M5.Btn.wasPressed()) {
+    FSM++;
+    if (FSM > 4)
+    {
+      FSM = 0;
+    }
+  }
+  
   M5.update();
 }
